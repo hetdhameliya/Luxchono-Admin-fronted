24dbc0aa290709f8105ec4103fb78f16bed62a69
hetdhameliya <106305428+hetdhameliya@users.noreply.github.com>
2024-01-06 10:40:11 +0530

    done react_app_base_url 2  done

--- src/api/Login.ts
+++ src/api/Login.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const LoginApi = createApi({
     reducerPath: 'loginApi',
     baseQuery: fetchBaseQuery({
-        baseUrl: "/api"
+        baseUrl: process.env.REACT_APP_BASE_URL
     }),
     endpoints: (builder: any) => ({
         login: builder.mutation({
